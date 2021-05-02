13357f1122d7c13679e40a8f6f437a1b7e199d98
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-02 10:11:24 +0530

    Update _menu.py

--- Harita/modules/_menu.py
+++ Harita/modules/_menu.py
@@ -20,7 +20,7 @@ pagenumber = db.pagenumber
 
 
 
-about = "**About Me**\n\nMy name is Titan, A powerful group management bot who can take care of your groups with automated simple regular admin actions!\n\n**My Software Version:** 2.0.1\n**Telethon Version:** 1.21.1\n\n**My Developer:**\n• @DeshadeethThisarana\n\nUpdates Channel: [Click Here](t.me/gangoffriendschannel)\nSupport Chat: [Click Here](t.me/gangoffriends)\n\nAnd finally thanks for Supporting me😘"
+about = "**About Me**\n\nMy name is Titan[🤖](https://telegra.ph/file/9175b634560b417d4114c.jpg), A powerful group management bot who can take care of your groups with automated simple regular admin actions!\n\n**My Software Version:** 2.0.1\n**Telethon Version:** 1.21.1\n\n**My Developer:**\n• @DeshadeethThisarana\n\nUpdates Channel: [Click Here](t.me/gangoffriendschannel)\nSupport Chat: [Click Here](t.me/gangoffriends)\n\nAnd finally thanks for Supporting me😘"
 ad_caption = "Hey! I am Titan, here to help you manage your groups! I perform most of the admin functions and make your group automated!\n\nJoin @GangofFriendschannel for updates.\n@GangOfFriends for help and support\n\nYou can checkout more about me via following buttons."
 pm_caption = "Hey there! My name is Titan - I'm a powerful group management bot Made to help you manage your groups easily!\n\nHit /help to find out more about me and unleash my full potential.\n\n"
 pmt = "Hello there! I'm Titan\nI'm a Telethon Based group management bot\n with a Much More features! Have a look\nat the following for an idea of some of \nthe things I can help you with.\n\nMain commands available:\n/start : Starts me, can be used to check i'm alive or not.\n/help : PM's you this message.\nExplore My Commands🙃."
